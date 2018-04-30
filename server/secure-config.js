c47342f988dd601b7990d9113683e934deac0200
min_kinggod <heeeee123@ajou.ac.kr>
2018-04-30 20:38:57 +0900

    signup기능

---
+++ server/secure-config.js
@@ -0,0 +1,14 @@
+var config = {};
+
+config.dbConfig = {
+    connectionLimit: 10,
+    host: '',
+    port: '',
+    user: '',
+    password: '',
+    database: '',
+    debug: false
+    
+}
+
+module.exports = config;
