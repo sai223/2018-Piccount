b56b23704a02977f261b8c4e666a5ac959d3bc68
ksos104 <ksos104@ajou.ac.kr>
2018-05-09 18:31:24 -0700

    Modify logIn.js form

--- server/routes/logIn.js
+++ server/routes/logIn.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var userAdapter = require('../adapters/user-adapter');
+
+const User = require('../models/user');
+
+/*
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -8,53 +13,80 @@ var connection = mysql.createConnection({
 	password: '',
 	database: 'piccount_server'
 });
+*/
 
-const logInSuccess = true;
-const logInFail = false;
+const logInSuccess = 'true';
+const logInFail = 'false';
 
 var userInfo = '';
 var userId;
 var userPassword;
 
-router.post('/logIn', function (req, res) {
+router.post('/', function (req, res) {
 
-	req.on('data', function(data) {
-		userInfo = JSON.parse(data);
+	if(req.body.id == undefined, req.body.password == undefined) {
+		return res.json({success : false});
+	} else {
+		userId = req.body.id;
+		userPassword = req.body.password;
 
-		userId = userInfo.id;
-		userPassword = userInfo.password;
-	});
-
-	req.on('end', function() {
-		connection.connect(function(err) {
-			if (err) throw err;
-			console.log('DB is not connected.');
-		});
-
-		connection.query('SELECT userPw FROM user_info WHERE userId = ' + userId, function(err, result) {
-	
-			if (err) throw err;
-			
-			var resJson = '';
-
-			if (result == NULL) {
-	
-				resJson = '{"success":"'+logInFail+'"}';
-				console.log('There is no such information!');
-	
-			} else if(result == userPassword) {
-
-				resJson = '{"success":"'+logInSuccess+'"}';	
-				console.log('logIn is completed!');
-	
+		userAdapter.search(userId, null, function(resultCode, rows) {
+			if(resultCode == "Fail") {
+				res.json({success: false});
 			} else {
-				
-				resJson = '{"success":"'+logInFail+'"}';
-				console.log('logIn is wrong information!');
+//				if(rows.length > 0) {
+					res.json({success: false});
+					resultCode == "Fail"
+				} else {
+					if(userPassword == rows.body.userPw){
+						res.json({success: true});
+					} else {
+						res.json({success: false});
+					}
+				}
 			}
-
-			res.send(resJson);
-	
-		});
-	});
+		}
+	}
 });
+
+
+//	req.on('data', function(data) {
+//		userInfo = JSON.parse(data);
+//
+//		userId = userInfo.id;
+//		userPassword = userInfo.password;
+//	});
+//
+//	req.on('end', function() {
+//		connection.connect(function(err) {
+//			if (err) throw err;
+//			console.log('DB is not connected.');
+//		});
+//
+//		connection.query('SELECT userPw FROM user_info WHERE userId = ' + userId, function(err, result) {
+//	
+//			if (err) throw err;
+//			
+//			var resJson = '';
+//
+//			if (result == NULL) {
+//	
+//				resJson = '{"success":"'+logInFail+'"}';
+//				console.log('There is no such information!');
+//	
+//			} else if(result == userPassword) {
+//
+//				resJson = '{"success":"'+logInSuccess+'"}';	
+//				console.log('logIn is completed!');
+//	
+//			} else {
+//				
+//				resJson = '{"success":"'+logInFail+'"}';
+//				console.log('logIn is wrong information!');
+//			}
+//
+//			res.send(resJson);
+//	
+//		});
+//	});
+//});
