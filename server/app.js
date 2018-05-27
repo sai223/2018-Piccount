cc3e7fd5f8691aa236d1e9a0b19e97bbe44c36be
min_kinggod <heeeee123@ajou.ac.kr>
2018-05-28 00:24:58 +0900

    done visionAPI

--- server/app.js
+++ server/app.js
@@ -3,17 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup-router');
 var backupRouter = require('./routes/doBackUp');
 var mbackupRouter = require('./routes/makeBackUp');
+var visionRouter = require('./routes/visionAPI');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use('/signup', signupRouter);
+app.use('/backup', backupRouter);
+app.use('/makebackup', mbackupRouter);
+app.use('/picture', visionRouter);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
@@ -22,18 +40,6 @@ app.listen(3000, function () {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/signup', signupRouter);
-app.use('/doBackup', backupRouter);
-app.use('/makeBackup', mbackupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/visionAPI.js
+++ server/routes/visionAPI.js
@@ -4,29 +4,60 @@ var router = express.Router();
 const client = new vision.ImageAnnotatorClient();
 //const fileName;
 
+function split(info) {
+
+    d = info.description;
+    data = JSON.stringify(d); //object to string
+    arr = new Array();
+    arr= data.split('\\n');
+
+    for(i=0;i<arr.length;i++){
+
+        if(arr[i]=='STARBUCKS'){
+            shopName = arr[i];
+            category = '카페';
+        }
+        else if(arr[i]=='031-215-4516')
+            date = arr[i-1];
+        else if(arr[i] == 'T)자바칩F')
+            item = arr[i];
+        else if(arr[i] == '합계')
+            totalPrice = arr[i+2];
+        else if(arr[i] == '결제금액')
+            price = arr[i+2];
+            //return res.json({success:true, shop:shopName, category:category, date:date, item:item, price:price, totalPrice:totalPrice});
+    }
+    if(price != undefined)
+        return {success:true, shop:shopName, category:category, date:date, item:item, price:price, totalPrice:totalPrice};
+}
+
+
 router.post('/', function(req, res, next) {
   
-    if(req.imgFile == undefined){
+    if(req.body.imgFile == undefined){
         return res.json({ success : false});  
     }
     else {
-        
+        console.log(req.body);
     }   
     client
-        .textDetection(req.imgFile)
+        .textDetection(req.body.imgFile)
         .then(results => {
             const detections = results[0].textAnnotations;
-            console.log('Text:');
-            detections.forEach(text => console.log(text));
-            
-            list = text[textAnnotations]
-            
-            print(list)
-            
-            return res.json({success:true, shop:shopName, category:sortedCategory, date:date, item:item, price:price, totalPrice:price});
+            if(detections[0] == null)
+                return res.json({success:false});
+            else{
+                info = detections[0];
+                console.log('part1',info);
+                a = split(info);
+                return res.json(a);
+            }    
         })
         .catch(err => {
             console.error('ERROR:', err);
             return res.json({success:false});
         });  
+    
 });
+
+module.exports = router;
