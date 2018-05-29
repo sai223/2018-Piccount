3293091d5ecad36fa685601ad531771e8dbc63b0
min_kinggod <heeeee123@ajou.ac.kr>
2018-05-29 20:18:55 +0900

    add vision API

--- server/routes/visionAPI.js
+++ server/routes/visionAPI.js
@@ -5,59 +5,121 @@ const client = new vision.ImageAnnotatorClient();
 //const fileName;
 
 function split(info) {
-
+    item = [];
+    price = [];
+    extra = [];
+    item_index = [];
+    num_extra = 0;
+    num_item = 0;
     d = info.description;
     data = JSON.stringify(d); //object to string
     arr = new Array();
-    arr= data.split('\\n');
-
-    for(i=0;i<arr.length;i++){
-
-        if(arr[i]=='STARBUCKS'){
-            shopName = arr[i];
-            category = '카페';
+    arr = data.split('\\n');
+    console.log(arr);
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf('STARBUCKS') != -1) {
+            if (arr[i].indexOf('"') != -1) {
+                shop = arr[i].split('"');
+                shopName = shop[1];
+                category = '카페'; 
+                
+            }
+            else {
+                shopName = arr[i];
+                category = '카페';
+            }
+        } else if (arr[i].indexOf('POS') != -1) {
+            console.log(arr[i].length);
+            if (arr[i].length == 17) {  //전자영수증일때
+                data = arr[i + 3].split(/(\s+)/);
+                date = data[0];
+            }
+            else {
+                data = arr[i].split(/(\s+)/);  //일반 영수증
+                for(k=0;k<data.length;k++)
+                    if(data[k].indexOf('2018-')!=-1)
+                //console.log(d[8]);
+                        date = data[k];
+            }
         }
-        else if(arr[i]=='031-215-4516')
-            date = arr[i-1];
-        else if(arr[i] == 'T)자바칩F')
-            item = arr[i];
-        else if(arr[i] == '합계')
-            totalPrice = arr[i+2];
-        else if(arr[i] == '결제금액')
-            price = arr[i+2];
-            //return res.json({success:true, shop:shopName, category:category, date:date, item:item, price:price, totalPrice:totalPrice});
+        else if (arr[i].indexOf('결제금액') != -1) {
+            totalPrice = arr[i + 2];
+        }
+        else if (arr[i].indexOf('G)') != -1 || arr[i].indexOf('샌드') != -1 || arr[i].indexOf('SW') != -1 || arr[i].indexOf('F') != -1 || arr[i].indexOf('-T') != -1) {
+            item.push(arr[i]);
+            console.log(item.length);
+            item_index.push(i);
+            num_item = item.length;
+        }
+        else if (arr[i].indexOf('L') != -1) {
+            extra.push(arr[i]);
+            num_extra = extra.length;
+            //price.push(arr[i + num_item + num_extra]);            
+        }
     }
-    if(price != undefined)
-        return {success:true, shop:shopName, category:category, date:date, item:item, price:price, totalPrice:totalPrice};
+        for(j=0;j<item_index.length;j++)
+            price.push(arr[item_index[j] + num_item + num_extra]);     
+            
+            
+        
+        /*
+        else if (arr[i].indexOf('SW') != -1){
+            item.push(arr[i]);
+            item.push(arr[i+1]);
+        }
+        else if (arr[i].indexOf('합계') != -1){
+            price = arr[i-2];
+        }
+        */
+
+        /*
+       
+        else if (arr[i].indexOf('POS') != -1) {
+            data = arr[i].split(/(\s+)/);
+            //console.log(d[8]);
+            date = data[8];
+        }
+        else if (arr[i].indexOf('주문') != -1) {
+            item = arr[i + 3];
+            price = arr[i + 1];
+        }
+        else if (arr[i] == '결제금액')
+            totalPrice = arr[i + 2];
+            */
+        //return res.json({success:true, shop:shopName, category:category, date:date, item:item, price:price, totalPrice:totalPrice});
+    
+
+    if (price != undefined)
+        return { success: true, shop: shopName, category: category, date: date, item: item, price: price, totalPrice: totalPrice };
 }
 
 
-router.post('/', function(req, res, next) {
-  
-    if(req.body.imgFile == undefined){
-        return res.json({ success : false});  
+router.post('/', function (req, res, next) {
+
+    if (req.body.imgFile == undefined) {
+        return res.json({ success: false });
     }
     else {
-        console.log(req.body);
-    }   
+        //console.log(req.body);
+    }
     client
         .textDetection(req.body.imgFile)
         .then(results => {
             const detections = results[0].textAnnotations;
-            if(detections[0] == null)
-                return res.json({success:false});
-            else{
+            if (detections[0] == null)
+                return res.json({ success: false });
+            else {
                 info = detections[0];
-                console.log('part1',info);
+                //console.log('part1',info);
                 a = split(info);
                 return res.json(a);
-            }    
+            }
         })
         .catch(err => {
             console.error('ERROR:', err);
-            return res.json({success:false});
-        });  
-    
+            return res.json({ success: false });
+        });
+
 });
 
 module.exports = router;
