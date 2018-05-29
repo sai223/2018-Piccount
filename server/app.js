cc1ea490e23ab700a5045b5e1aeee386add2128e
ksos104 <ksos104@ajou.ac.kr>
2018-05-29 04:14:53 -0700

    Local Test base64 decode

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,14 +13,13 @@ var signupRouter = require('./routes/signup-router');
 var backupRouter = require('./routes/doBackUp');
 var mbackupRouter = require('./routes/makeBackUp');
 var logInRouter = require('./routes/logIn');
+
 var visionRouter = require('./routes/visionAPI');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,11 +28,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/signup', signupRouter);
+app.use('/signUp', signupRouter);
 app.use('/doBackup', backupRouter);
 app.use('/makeBackup', mbackupRouter);
 app.use('/logIn', logInRouter);
 app.use('/visionAPI', visionRouter);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
